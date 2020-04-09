8657c98e57f9b0dee903f23c45b43acf893ce482
aakalaew <arthur.akalaew@web.de>
2020-04-09 10:24:54 -0700

    fix syntax

--- mobile/android/build_android_application.sh
+++ mobile/android/build_android_application.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #METEOR_PROJECT_PATH=$(pwd)
-METEOR_PROJECT_PATH = ./simple-todos
+METEOR_PROJECT_PATH=./simple-todos
 SERVER="${APPLICATION_NAME}.meteor.com"
 APPLICATION_NAME="$(basename "$METEOR_PROJECT_PATH")_Android_Application"
 
