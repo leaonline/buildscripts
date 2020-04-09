037b65451dfd79d5054dd7d672f73cc6113f5db4
aakalaew <arthur.akalaew@web.de>
2020-04-09 10:50:01 -0700

    final fixes hopefully

--- mobile/android/build_android_application.sh
+++ mobile/android/build_android_application.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-METEOR_PROJECT_PATH=$(pwd) # /home/runner/work/buildscripts/buildscripts
-cd ../simple-todos || exit
-echo $(pwd)
+METEOR_PROJECT_PATH=$(pwd)/simple-todos # /home/runner/work/buildscripts/buildscripts
+cd simple-todos || exit
 SERVER="${APPLICATION_NAME}.meteor.com"
 APPLICATION_NAME="$(basename "$METEOR_PROJECT_PATH")_Android_Application"
 
-cd "${METEOR_PROJECT_PATH}"/.. || exit # go back to root so android project will create on root and not in script folder
+#cd "${METEOR_PROJECT_PATH}"/.. || exit # go back to root so android project will create on root and not in script folder
 
 if [ -d "${APPLICATION_NAME}" ]; then  # check if apk build already exists, if not continue. 
 				     # If android build already exists remove it, otherwise it will throw errors. 
@@ -14,6 +13,8 @@ if [ -d "${APPLICATION_NAME}" ]; then  # check if apk build already exists, if n
 
 fi
 
+
+echo $(pwd)
 meteor build "${APPLICATION_NAME}" --server="${SERVER}"
 
 
