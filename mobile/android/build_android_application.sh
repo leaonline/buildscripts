3fc053580023b68bab35edc5813923d956f777d7
Arthur <arthur.akalaew@web.de>
2020-04-12 20:55:27 +0200

    add platform function added

--- mobile/android/build_android_application.sh
+++ mobile/android/build_android_application.sh
@@ -7,6 +7,8 @@ APPLICATION_NAME="$(basename "$METEOR_PROJECT_PATH")_Android_Application"
 
 #cd "${METEOR_PROJECT_PATH}"/.. || exit # go back to root so android project will create on root and not in script folder
 
+
+
 if [ -d "${APPLICATION_NAME}" ]; then  # check if apk build already exists, if not continue. 
 				     # If android build already exists remove it, otherwise it will throw errors. 
   sudo rm -rf "${APPLICATION_NAME}" 
@@ -14,7 +16,7 @@ if [ -d "${APPLICATION_NAME}" ]; then  # check if apk build already exists, if n
 fi
 
 
-echo $(pwd)
+meteor add-platform android
 meteor build "${APPLICATION_NAME}" --server="${SERVER}"
 
 
