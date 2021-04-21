4a386ee9c79ab7b578147d0616682766e0f60053
Jan Küster <jkuester@uni-bremen.de>
2021-04-21 11:23:28 +0200

    fonts convertOtf.sh added

---
+++ fonts/convertOtf.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -e
+
+# We need this script to convert our otf fonts to multiple web font formats
+# in order to support most browsers out-of-the-box
+
+# 1. check and prepare variables and fail if the parameters are not configured
+# correctly. We need an .otf file as input and a target folder as output
+SOURCE=$1
+DESTINATION=$2
+FILENAME=$(basename "${SOURCE%.otf}")
+
+# we omit mime check since we use this script only locally and assume it's otf
+if [[ ${SOURCE: -4} != ".otf" ]]; then
+  echo "usage: convertOtf input.otf /path/to/output/"
+  exit 1
+fi
+
+# check the destination is a folder
+if [[ ! -d "$DESTINATION" ]]; then
+  echo "usage: convertOtf input.otf /path/to/output/"
+  exit 1
+fi
+
+# check if all tools are installed. We need all tools to be installed in order
+allCommands=(
+  mkeot
+  eot2ttf
+  sfnt2woff
+  woff2_compress
+)
+
+for i in "${allCommands[@]}"
+do
+   :
+   if [[! command -v "$i" &> "/dev/null"]];then
+        echo "Cannot convert ($i). The following packages are required: eot-utils eot2ttf woff-tools woff2"
+        exit 1
+    fi
+done
+exit 1
+
+# EOT EXPORT
+EOT_PATH="$DESTINATION/$FILENAME.eot"
+echo "[OTF -> EOT]: (over-)write to $EOT_PATH"
+mkeot ${SOURCE} > ${EOT_PATH}
+
+# TTF EXPORT
+TTF_PATH="$DESTINATION/$FILENAME.ttf"
+echo "[EOT -> TTF]: (over-)write to $TTF_PATH"
+eot2ttf ${EOT_PATH} ${TTF_PATH}
+
+# WOFF export
+WOFF_PATH="$DESTINATION/$FILENAME.woff"
+echo "[OTF -> WOFF]: (over-)write to $WOFF_PATH"
+sfnt2woff ${SOURCE} > ${WOFF_PATH}
+
+# WOFF2 export
+WOFF2_PATH="$DESTINATION/$FILENAME.woff2"
+echo "[TTF -> WOFF2]: (over-)write to $WOFF2_PATH"
+woff2_compress ${TTF_PATH} > ${WOFF2_PATH}
